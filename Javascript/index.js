904897e5ce7719d0cccffb37f7b5fb1765a8893d
vjroll-180 <vjroll180@gmail.com>
2024-03-01 13:23:19 +0000

    Remove console bug due to default cities

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,25 +1,29 @@
 function updateTimezone() {
   let londonElement = document.querySelector("#london");
-  let londonDateElement = londonElement.querySelector(".city-date");
-  let londonTimeElement = londonElement.querySelector(".city-time");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".city-date");
+    let londonTimeElement = londonElement.querySelector(".city-time");
 
-  londonDateElement.innerHTML = moment()
-    .tz("Europe/London")
-    .format("MMMM Do YYYY");
-  londonTimeElement.innerHTML = moment()
-    .tz("Europe/London")
-    .format("hh:mm:ss [<small>] A [</small>]");
+    londonDateElement.innerHTML = moment()
+      .tz("Europe/London")
+      .format("MMMM Do YYYY");
+    londonTimeElement.innerHTML = moment()
+      .tz("Europe/London")
+      .format("hh:mm:ss [<small>] A [</small>]");
+  }
 
   let stockholmElement = document.querySelector("#stockholm");
-  let stockholmDateElement = stockholmElement.querySelector(".city-date");
-  let stockholmTimeElement = stockholmElement.querySelector(".city-time");
+  if (stockholmElement) {
+    let stockholmDateElement = stockholmElement.querySelector(".city-date");
+    let stockholmTimeElement = stockholmElement.querySelector(".city-time");
 
-  stockholmDateElement.innerHTML = moment()
-    .tz("Europe/Stockholm")
-    .format("MMMM Do YYYY");
-  stockholmTimeElement.innerHTML = moment()
-    .tz("Europe/Stockholm")
-    .format("hh:mm:ss [<small>] A [</small>]");
+    stockholmDateElement.innerHTML = moment()
+      .tz("Europe/Stockholm")
+      .format("MMMM Do YYYY");
+    stockholmTimeElement.innerHTML = moment()
+      .tz("Europe/Stockholm")
+      .format("hh:mm:ss [<small>] A [</small>]");
+  }
 }
 
 function updateCity(event) {
