9f5d7318779462fac609eae62cb58bc6a38647eb
vjroll-180 <vjroll180@gmail.com>
2024-03-01 11:37:27 +0000

    Add date&time using moment.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,8 @@
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(".city-date");
+let londonTimeElement = londonElement.querySelector(".city-time");
 
+londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+londonTimeElement.innerHTML = moment().format(
+  "hh:mm:ss [<small>] A [</small>]"
+);
