5a25f4e832c852e0b8ce26fdec2c2aaf73b4e119
vjroll-180 <vjroll180@gmail.com>
2024-03-01 12:15:50 +0000

    Added moment.tz.js to second city

--- Javascript/index.js
+++ Javascript/index.js
@@ -9,6 +9,17 @@ function updateTimezone() {
   londonTimeElement.innerHTML = moment()
     .tz("Europe/London")
     .format("hh:mm:ss [<small>] A [</small>]");
+
+  let stockholmElement = document.querySelector("#stockholm");
+  let stockholmDateElement = stockholmElement.querySelector(".city-date");
+  let stockholmTimeElement = stockholmElement.querySelector(".city-time");
+
+  stockholmDateElement.innerHTML = moment()
+    .tz("Europe/Stockholm")
+    .format("MMMM Do YYYY");
+  stockholmTimeElement.innerHTML = moment()
+    .tz("Europe/Stockholm")
+    .format("hh:mm:ss [<small>] A [</small>]");
 }
 
 setInterval(updateTimezone, 1000);
