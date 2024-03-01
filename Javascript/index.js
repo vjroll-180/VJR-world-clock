7f7c6a9a2a39c37ad5622ea37b527bc2e2239993
vjroll-180 <vjroll180@gmail.com>
2024-03-01 11:44:29 +0000

    Add moment.tz.js and update js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,8 +1,14 @@
-let londonElement = document.querySelector("#london");
-let londonDateElement = londonElement.querySelector(".city-date");
-let londonTimeElement = londonElement.querySelector(".city-time");
+function updateTimezone() {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".city-date");
+  let londonTimeElement = londonElement.querySelector(".city-time");
 
-londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
-londonTimeElement.innerHTML = moment().format(
-  "hh:mm:ss [<small>] A [</small>]"
-);
+  londonDateElement.innerHTML = moment()
+    .tz("Europe/London")
+    .format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = moment()
+    .tz("Europe/London")
+    .format("hh:mm:ss [<small>] A [</small>]");
+}
+
+setInterval(updateTimezone, 1000);
